e6710c3d7225a64afad71d3b9b6dcf4c6557c810
Baboo Bhaiya <baboobhaiya@protonmail.com>
2021-10-06 11:43:53 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2becdd4112058b7036c4d0036885a405.js"
+  "./precache-manifest.7201c72adc8f725e084166d001cad3e5.js"
 );
 
 self.addEventListener('message', (event) => {
