fc87e878dd8c7a96ecea40bab3e980dd2eacebe9
0xdon <0xdon@protonmail.com>
2021-10-11 23:29:01 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6851202dae02bc22f42d8c6def79e584.js"
+  "./precache-manifest.760012dc988b27bd82596fb47e3c611c.js"
 );
 
 self.addEventListener('message', (event) => {
