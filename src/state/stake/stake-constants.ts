a245a064b2e3819e2066442aebce76753ad7d66e
0xdon <0xdon@protonmail.com>
2021-11-09 18:29:14 +0000

    resolved page reload state issue

--- src/state/stake/stake-constants.ts
+++ src/state/stake/stake-constants.ts
@@ -1,107 +1,111 @@
-import { ChainId, Token, TokenAmount } from "@trisolaris/sdk";
-import {
-    USDC,
-    AAVE,
-    DAI,
-    ZERO_ADDRESS
-  } from '../../constants'
+import { ChainId, Token, TokenAmount } from '@trisolaris/sdk'
+import { USDC, AAVE, DAI, ZERO_ADDRESS } from '../../constants'
 
 export interface StakingTri {
-    ID: number
-    tokens: [Token, Token]
-    stakingRewardAddress: string
-    isPeriodFinished: boolean
-    stakedAmount: TokenAmount | undefined,
-    earnedAmount: TokenAmount | undefined,
-    totalStakedAmount: TokenAmount | undefined,
-    totalStakedAmountInUSD: TokenAmount | undefined,
-    totalStakedAmountInETH: TokenAmount | undefined,
-    // the amount of token distributed per second to all LPs, constant
-    totalRewardRate: TokenAmount | undefined,
-    // the current amount of token distributed to the active account per second.
-    // equivalent to percent of total supply * reward rate
-    rewardRate: TokenAmount | undefined,
-    apr: number,
-  }
+  ID: number
+  tokens: [Token, Token]
+  stakingRewardAddress: string
+  isPeriodFinished: boolean
+  stakedAmount: TokenAmount
+  earnedAmount: TokenAmount
+  totalStakedAmount: TokenAmount
+  totalStakedAmountInUSD: TokenAmount
+  totalStakedAmountInETH: TokenAmount
+  // the amount of token distributed per second to all LPs, constant
+  totalRewardRate: TokenAmount
+  // the current amount of token distributed to the active account per second.
+  // equivalent to percent of total supply * reward rate
+  rewardRate: TokenAmount
+  apr: number
+}
 
-export const TRI = new Token(ChainId.POLYGON, "0x2CB45Edb4517d5947aFdE3BEAbF95A582506858B", 18, 'TRI', 'Trisolaris')
+const dummyToken = new Token(ChainId.POLYGON, ZERO_ADDRESS, 18, 'ZERO', 'ZERO')
+
+// const dummyPair = new Pair(
+//   new TokenAmount(dummyToken, '0'),
+//   new TokenAmount(dummyToken, '0'),
+//   chainId ? chainId : ChainId.POLYGON
+// )
+const dummyAmount = new TokenAmount(dummyToken, '0')
+
+export const TRI = new Token(ChainId.POLYGON, '0x2CB45Edb4517d5947aFdE3BEAbF95A582506858B', 18, 'TRI', 'Trisolaris')
 
 const POLYGON_POOLS: StakingTri[] = [
-    {
-        ID: 0,
-        tokens: [DAI[ChainId.POLYGON], USDC[ChainId.POLYGON]],
-        stakingRewardAddress: "0xd6f922f6eB4dfa47f53C038c7dE9bE614a49257f",
-        isPeriodFinished: false,
-        stakedAmount: undefined,
-        earnedAmount: undefined,
-        totalStakedAmount: undefined,
-        totalStakedAmountInUSD: undefined,
-        totalStakedAmountInETH: undefined,
-        totalRewardRate: undefined,
-        rewardRate: undefined,
-        apr: 10,
-    },
-    {
-        ID: 1,
-        tokens: [AAVE[ChainId.POLYGON], DAI[ChainId.POLYGON]],
-        stakingRewardAddress: "0x76F4128B11f429289499BA29518Ef7E5b26025B6",
-        isPeriodFinished: false,
-        stakedAmount: undefined,
-        earnedAmount: undefined,
-        totalStakedAmount: undefined,
-        totalStakedAmountInUSD: undefined,
-        totalStakedAmountInETH: undefined,
-        totalRewardRate: undefined,
-        rewardRate: undefined,
-        apr: 10,
-    },
-    {
-        ID: 2,
-        tokens: [TRI, USDC[ChainId.POLYGON]],
-        stakingRewardAddress: "0xb0c5eFFD0eA4D4d274971374d696Fa08860Ea709",
-        isPeriodFinished: false,
-        stakedAmount: undefined,
-        earnedAmount: undefined,
-        totalStakedAmount: undefined,
-        totalStakedAmountInUSD: undefined,
-        totalStakedAmountInETH: undefined,
-        totalRewardRate: undefined,
-        rewardRate: undefined,
-        apr: 10,
-    },
+  {
+    ID: 0,
+    tokens: [DAI[ChainId.POLYGON], USDC[ChainId.POLYGON]],
+    stakingRewardAddress: '0xd6f922f6eB4dfa47f53C038c7dE9bE614a49257f',
+    isPeriodFinished: false,
+    stakedAmount: dummyAmount,
+    earnedAmount: dummyAmount,
+    totalStakedAmount: dummyAmount,
+    totalStakedAmountInUSD: dummyAmount,
+    totalStakedAmountInETH: dummyAmount,
+    totalRewardRate: dummyAmount,
+    rewardRate: dummyAmount,
+    apr: 10
+  },
+  {
+    ID: 1,
+    tokens: [AAVE[ChainId.POLYGON], DAI[ChainId.POLYGON]],
+    stakingRewardAddress: '0x76F4128B11f429289499BA29518Ef7E5b26025B6',
+    isPeriodFinished: false,
+    stakedAmount: dummyAmount,
+    earnedAmount: dummyAmount,
+    totalStakedAmount: dummyAmount,
+    totalStakedAmountInUSD: dummyAmount,
+    totalStakedAmountInETH: dummyAmount,
+    totalRewardRate: dummyAmount,
+    rewardRate: dummyAmount,
+    apr: 10
+  },
+  {
+    ID: 2,
+    tokens: [TRI, USDC[ChainId.POLYGON]],
+    stakingRewardAddress: '0xb0c5eFFD0eA4D4d274971374d696Fa08860Ea709',
+    isPeriodFinished: false,
+    stakedAmount: dummyAmount,
+    earnedAmount: dummyAmount,
+    totalStakedAmount: dummyAmount,
+    totalStakedAmountInUSD: dummyAmount,
+    totalStakedAmountInETH: dummyAmount,
+    totalRewardRate: dummyAmount,
+    rewardRate: dummyAmount,
+    apr: 10
+  }
 ]
 
 const NULL_POOLS: StakingTri[] = [
-    {
-        ID: 0,
-        tokens: [
-            new Token(ChainId.FUJI, ZERO_ADDRESS, 18, 'ZERO', 'ZERO'),
-            new Token(ChainId.FUJI, ZERO_ADDRESS, 18, 'ZERO', 'ZERO')],
-        stakingRewardAddress: ZERO_ADDRESS,
-        isPeriodFinished: false,
-        stakedAmount: undefined,
-        earnedAmount: undefined,
-        totalStakedAmount: undefined,
-        totalStakedAmountInUSD: undefined,
-        totalStakedAmountInETH: undefined,
-        totalRewardRate: undefined,
-        rewardRate: undefined,
-        apr: 0,
-    }
+  {
+    ID: 0,
+    tokens: [
+      new Token(ChainId.FUJI, ZERO_ADDRESS, 18, 'ZERO', 'ZERO'),
+      new Token(ChainId.FUJI, ZERO_ADDRESS, 18, 'ZERO', 'ZERO')
+    ],
+    stakingRewardAddress: ZERO_ADDRESS,
+    isPeriodFinished: false,
+    stakedAmount: dummyAmount,
+    earnedAmount: dummyAmount,
+    totalStakedAmount: dummyAmount,
+    totalStakedAmountInUSD: dummyAmount,
+    totalStakedAmountInETH: dummyAmount,
+    totalRewardRate: dummyAmount,
+    rewardRate: dummyAmount,
+    apr: 0
+  }
 ]
 
 export const STAKING: {
-    [chainid in ChainId] : StakingTri[]
-  } = {
-    
-    [ChainId.FUJI]: NULL_POOLS,
-    [ChainId.AVALANCHE]: NULL_POOLS,
-    [ChainId.POLYGON]: POLYGON_POOLS,
-    [ChainId.AURORA]: NULL_POOLS,
-  }
+  [chainid in ChainId]: StakingTri[]
+} = {
+  [ChainId.FUJI]: NULL_POOLS,
+  [ChainId.AVALANCHE]: NULL_POOLS,
+  [ChainId.POLYGON]: POLYGON_POOLS,
+  [ChainId.AURORA]: NULL_POOLS
+}
 
-export const ADDRESS_PRICE_MAP: {[key: string]: string} = {
-    "0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063": "dai",
-    "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174": "usd-coin",
-    "0xD6DF932A45C0f255f85145f286eA0b292B21C90B": "aave",
+export const ADDRESS_PRICE_MAP: { [key: string]: string } = {
+  '0x8f3Cf7ad23Cd3CaDbD9735AFf958023239c6A063': 'dai',
+  '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174': 'usd-coin',
+  '0xD6DF932A45C0f255f85145f286eA0b292B21C90B': 'aave'
 }
