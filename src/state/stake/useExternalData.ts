a6a33f41b7224d0db6327117947d758662284f6a
0xchain1 <0xchain1@protonmail.com>
2021-11-14 11:29:53 -0500

    constants

--- src/state/stake/useExternalData.ts
+++ src/state/stake/useExternalData.ts
@@ -21,41 +21,6 @@ const useExternalDataService = () => {
   return [result];
 };
 
-// const useExternalDataService = ExternalStakeConstants {
-//     useEffect(() => {
-//     fetch('https://raw.githubusercontent.com/trisolaris-labs/apr/master/data.json')
-//       .then(results => results.json())
-//       .then(data => {
-//         setStakingInfoData(data)
-//       })
-//   }, [])
-
-// }
-
-// const useExternalDataService = () => {
-//   const [result, setResult] = useState<Service<ExternalStakeConstants[]>>({
-//     status: 'loading'
-//   });
-
-//   useEffect(() => {
-//     fetch('https://raw.githubusercontent.com/trisolaris-labs/apr/master/data.json')
-//       .then(response => response.json())
-//       .then(response => setResult({ status: 'loaded', payload: response }))
-//       .catch(error => setResult({ status: 'error', error }));
-//   }, []);
-
-//   return result;
-// };
-
-
-//   useEffect(() => {
-//     fetch('https://raw.githubusercontent.com/trisolaris-labs/apr/master/data.json')
-//       .then(results => results.json())
-//       .then(data => {
-//         setStakingInfoData(data)
-//       })
-//   }, [])
-
-
+// mapping object with id, lpaddress
 
 export default useExternalDataService;
