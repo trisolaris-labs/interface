89dc42eed61f164affb90c2a727abbc8a1eb612d
0xdon <0xdon@protonmail.com>
2021-11-14 18:55:40 +0000

    intermediate state of mapping result to tobject

--- src/state/stake/useExternalData.ts
+++ src/state/stake/useExternalData.ts
@@ -1,23 +1,38 @@
 import { useEffect, useState } from 'react'
-import { ExternalStakeConstant, Service } from './external-stake-constants'
+import { ExternalStakeConstant, Service, STAKING_TOKEN_LIST } from './external-stake-constants'
+
 
 export interface ExternalStakeConstants {
   results: ExternalStakeConstant[]
 }
 
 const useExternalDataService = () => {
-  const [result, setResult] = useState<Service<ExternalStakeConstants[]>>({
-    status: 'loading'
-  })
+  const [result, setResult] = useState<Service<ExternalStakeConstants[]>>()
 
   useEffect(() => {
     fetch('https://raw.githubusercontent.com/trisolaris-labs/apr/master/data.json')
       .then(response => response.json())
-      .then(response => setResult({ status: 'loaded', payload: response }))
-      .catch(error => setResult({ status: 'error', error }))
+      .then(response => {
+        const parsedResponseArr = response.map(
+          (responseobj: ExternalStakeConstant) =>
+            ({
+              id: responseobj.id,
+              lpAddress: responseobj.lpAddress,
+              totalSupply: responseobj.totalSupply,
+              totalStaked: responseobj.totalStaked,
+              totalStakedInUSD: responseobj.totalStakedInUSD,
+              totalRewardRate: responseobj.totalRewardRate,
+              allocPoint: responseobj.allocPoint,
+              apr: responseobj.apr,
+              token0: STAKING_TOKEN_LIST['0x20F8AeFB5697B77E0BB835A8518BE70775cdA1b0'][0],
+              token1: STAKING_TOKEN_LIST['0x20F8AeFB5697B77E0BB835A8518BE70775cdA1b0'][1]
+            } as ExternalStakeConstant)
+        )
+        setResult(parsedResponseArr)
+      })
   }, [])
 
-  return [result]
+  return result
 }
 
 // mapping object with id, lpaddress
