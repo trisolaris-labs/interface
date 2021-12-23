44c6a9c79732549686536e059d0229b549786f64
specialist0x <specialist0x@protonmail.com>
2021-12-23 08:43:21 -0800

    Updated polling interval

--- src/utils/getLibrary.ts
+++ src/utils/getLibrary.ts
@@ -1,7 +1,33 @@
 import { Web3Provider } from '@ethersproject/providers'
+import { ChainId } from '@trisolaris/sdk'
+
+const DEFAULT_POLLING_INTERVAL = 15_000; // 15 seconds
+
+const NETWORK_POLLING_INTERVALS: { [chainId: number]: number } = {
+  [ChainId.AURORA]: 1_000, // 1 second
+  [ChainId.AVALANCHE]: 1_000, // 1 second
+  [ChainId.POLYGON]: DEFAULT_POLLING_INTERVAL,
+}
 
 export default function getLibrary(provider: any): Web3Provider {
-  const library = new Web3Provider(provider, 'any')
-  library.pollingInterval = 15000
+  const library = new Web3Provider(
+    provider,
+    typeof provider.chainId === 'number'
+      ? provider.chainId
+      : typeof provider.chainId === 'string'
+        ? parseInt(provider.chainId)
+        : 'any'
+  )
+
+  library.pollingInterval = DEFAULT_POLLING_INTERVAL;
+
+  library.detectNetwork().then((network) => {
+    const networkPollingInterval = NETWORK_POLLING_INTERVALS[network.chainId]
+    if (networkPollingInterval) {
+      console.debug('Setting polling interval', networkPollingInterval)
+      library.pollingInterval = networkPollingInterval
+    }
+  })
+
   return library
 }
