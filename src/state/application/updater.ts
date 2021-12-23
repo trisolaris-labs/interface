8aa93fa8cbb05848310fe5f90e90d061bc37d3b8
specialist0x <specialist0x@protonmail.com>
2021-12-23 08:55:06 -0800

    Added useTimeout; updated application/updater to force dispatch

--- src/state/application/updater.ts
+++ src/state/application/updater.ts
@@ -1,9 +1,12 @@
 import { useCallback, useEffect, useState } from 'react'
 import { useActiveWeb3React } from '../../hooks'
-import useDebounce from '../../hooks/useDebounce'
 import useIsWindowVisible from '../../hooks/useIsWindowVisible'
 import { updateBlockNumber } from './actions'
 import { useDispatch } from 'react-redux'
+import useTimeout from '../../hooks/useTimeout';
+import useDebounce from '../../hooks/useDebounce'
+
+const MAX_WAIT_BEFORE_MANUAL_DISPATCH = 2000;
 
 export default function Updater(): null {
   const { library, chainId } = useActiveWeb3React()
@@ -14,9 +17,9 @@ export default function Updater(): null {
   const [state, setState] = useState<{ chainId: number | undefined; blockNumber: number | null }>({
     chainId,
     blockNumber: null
-  })
+  });
+  const debouncedState = useDebounce(state, 100);
 
-  const [hasChainID, setHasChainID] = useState<boolean>(chainId != null);
   const blockNumberCallback = useCallback(
     (blockNumber: number) => {
       setState(state => {
@@ -30,11 +33,21 @@ export default function Updater(): null {
     [chainId, setState]
   )
 
-  useEffect(() => { 
-    if (chainId != null && !hasChainID) {
-      setHasChainID(true);
+  // If the window's visible, there's a library, and a chainID, 
+  // and we still don't have a blocknumber after `MAX_WAIT_BEFORE_MANUAL_DISPATCH` seconds
+  // Force an update!
+  const setDelayTimeout = useCallback(() => {
+    if (!debouncedState.blockNumber && windowVisible && library && chainId && state.blockNumber) {
+      dispatch(updateBlockNumber({ chainId, blockNumber: state.blockNumber }));
     }
-  }, [chainId, hasChainID, setHasChainID])
+  }, [
+    library,
+    chainId,
+    debouncedState.blockNumber,
+    windowVisible,
+    state.blockNumber,
+  ]);
+  useTimeout(setDelayTimeout, MAX_WAIT_BEFORE_MANUAL_DISPATCH);
 
   // attach/detach listeners
   useEffect(() => {
@@ -50,16 +63,15 @@ export default function Updater(): null {
       .catch(error => console.error(`Failed to get block number for chainId: ${chainId}`, error))
 
     library.on('block', blockNumberCallback)
+    
     return () => {
       library.removeListener('block', blockNumberCallback)
     }
-  }, [dispatch, chainId, library, blockNumberCallback, windowVisible])
-
-  const debouncedState = useDebounce(state, 100)
+  }, [dispatch, chainId, library, blockNumberCallback, windowVisible]);
 
   useEffect(() => {
     if (!debouncedState.chainId || !debouncedState.blockNumber || !windowVisible) return
-    dispatch(updateBlockNumber({ chainId: debouncedState.chainId, blockNumber: debouncedState.blockNumber }))
+    dispatch(updateBlockNumber({ chainId: debouncedState.chainId, blockNumber: debouncedState.blockNumber }));
   }, [windowVisible, dispatch, debouncedState.blockNumber, debouncedState.chainId])
 
   return null
