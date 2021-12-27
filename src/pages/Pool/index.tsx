bb2da4ffec3d8faaa7b29c0602d981edfef26a25
0xchain1 <0xchain1@protonmail.com>
2021-12-26 20:25:04 -0500

    removed console log

--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -104,7 +104,6 @@ export default function Pool() {
       ),
     [tokenPairsWithLiquidityTokens, v2PairsBalances]
   )
-  console.log(liquidityTokensWithBalances)
   const v2Pairs = usePairs(liquidityTokensWithBalances.map(({ tokens }) => tokens))
   const v2IsLoading =
     fetchingV2PairBalances || v2Pairs?.length < liquidityTokensWithBalances.length || v2Pairs?.some(V2Pair => !V2Pair)
