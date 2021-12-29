1c181c94d6dc90e27a541ebe101dd1957e37f919
0xchain1 <0xchain1@protonmail.com>
2021-12-29 17:03:56 -0500

    remove ununsed addition

--- src/state/application/actions.ts
+++ src/state/application/actions.ts
@@ -29,7 +29,6 @@ export enum ApplicationModal {
   VOTE,
   LANGUAGE,
   TRI_PRICE,
-  BRIDGE
 }
 
 export const updateBlockNumber = createAction<{ chainId: number; blockNumber: number }>('application/updateBlockNumber')
