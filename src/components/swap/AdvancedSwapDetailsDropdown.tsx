5a5c1734b8efa1e51cd4ef46d2d84116cd1df154
specialist0x <specialist0x@protonmail.com>
2022-01-03 10:55:11 -0800

    Removed question marks from trade routing

--- src/components/swap/AdvancedSwapDetailsDropdown.tsx
+++ src/components/swap/AdvancedSwapDetailsDropdown.tsx
@@ -23,10 +23,8 @@ export default function AdvancedSwapDetailsDropdown({ trade, ...rest }: Advanced
   const lastTrade = useLastTruthy(trade)
 
   return (
-    <div> {/* This div is needed to maintain event capturing for nested nodes (eg mouseover events) */}
-      <AdvancedDetailsFooter show={Boolean(trade)}>
-        <AdvancedSwapDetails {...rest} trade={trade ?? lastTrade ?? undefined} />
-      </AdvancedDetailsFooter>
-    </div>
+    <AdvancedDetailsFooter show={Boolean(trade)}>
+      <AdvancedSwapDetails {...rest} trade={trade ?? lastTrade ?? undefined} />
+    </AdvancedDetailsFooter>
   )
 }
