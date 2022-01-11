910667ab4349e54ced47b9f1636e2fea90772356
memacius32 <memacius32@protonmail.com>
2022-01-11 02:07:53 -0300

    Add pools search filter. Add optimizations

--- src/components/earn/FarmBanner.tsx
+++ src/components/earn/FarmBanner.tsx
@@ -1,32 +1,31 @@
 import React from 'react'
-import styled from 'styled-components';
-import { TYPE } from '../../theme';
-import Card from '../Card';
-import { AutoRow } from '../Row';
-import spacemanOnPlanet from '../../assets/svg/spaceman_on_planet.svg';
-import { AutoColumn } from '../Column';
-import { useFarms } from '../../state/stake/apr';
-import { addCommasToNumber } from '../../utils';
-import { JSBI } from '@trisolaris/sdk';
+import styled from 'styled-components'
+import { TYPE } from '../../theme'
+import Card from '../Card'
+import { AutoRow } from '../Row'
+import spacemanOnPlanet from '../../assets/svg/spaceman_on_planet.svg'
+import { AutoColumn } from '../Column'
+import { useFarms } from '../../state/stake/apr'
+import { addCommasToNumber } from '../../utils'
+import { JSBI } from '@trisolaris/sdk'
 
 const ZERO = JSBI.BigInt(0)
 
 const StyledCard = styled(Card)`
-    background: ${({theme}) => 
-      `radial-gradient(farthest-corner at 0% 0%, ${theme.primary1} 0%, transparent 70%)`};
-    padding-right: 0;
-    padding-top: 0;
-    padding-bottom: 0;
+  background: ${({ theme }) => `radial-gradient(farthest-corner at 0% 0%, ${theme.primary1} 0%, transparent 70%)`};
+  padding-right: 0;
+  padding-top: 0;
+  padding-bottom: 0;
 `
 
 const IconContainer = styled.div`
-    max-height: 144px;
-    max-width: 240px;
-    overflow: hidden;
-    & > * {
-        object-position: 16px 16px;
-    }
-    ${({ theme }) => theme.mediaWidth.upToSmall`
+  max-height: 144px;
+  max-width: 240px;
+  overflow: hidden;
+  & > * {
+    object-position: 16px 16px;
+  }
+  ${({ theme }) => theme.mediaWidth.upToSmall`
         max-height: 90px;
         & > * {
             height: 200px;
@@ -37,30 +36,20 @@ const IconContainer = styled.div`
 `
 
 export default function FarmBanner() {
-    const farmTVL = useFarms()
-        .reduce(
-            (acc, farm) => JSBI.add(acc, JSBI.BigInt(farm.totalStakedInUSD)), 
-            ZERO,
-        );
-    const farmTVLFriendly = JSBI.GE(farmTVL, ZERO) 
-        ? `$${addCommasToNumber(farmTVL.toString())}` 
-        : '-'
+  const farmTVL = useFarms().reduce((acc, farm) => JSBI.add(acc, JSBI.BigInt(farm.totalStakedInUSD)), ZERO)
+  const farmTVLFriendly = JSBI.GE(farmTVL, ZERO) ? `$${addCommasToNumber(farmTVL.toString())}` : '-'
 
-    return (
-        <StyledCard>
-            <AutoRow justifyContent="space-between" style={{ alignItems: 'flex-start' }}>
-                <AutoColumn style={{ paddingTop: '1rem' }}>
-                    <TYPE.largeHeader>
-                        Farm
-                    </TYPE.largeHeader>
-                    <TYPE.subHeader marginTop="1rem">
-                        TVL: {farmTVLFriendly}
-                    </TYPE.subHeader>
-                </AutoColumn>
-                <IconContainer>
-                    <img height="360px" src={spacemanOnPlanet} />
-                </IconContainer>
-            </AutoRow>
-        </StyledCard>
-    );
+  return (
+    <StyledCard>
+      <AutoRow justifyContent="space-between" style={{ alignItems: 'flex-start' }}>
+        <AutoColumn style={{ paddingTop: '1rem' }}>
+          <TYPE.largeHeader>Farm</TYPE.largeHeader>
+          <TYPE.subHeader marginTop="1rem">TVL: {farmTVLFriendly}</TYPE.subHeader>
+        </AutoColumn>
+        <IconContainer>
+          <img height="360px" src={spacemanOnPlanet} />
+        </IconContainer>
+      </AutoRow>
+    </StyledCard>
+  )
 }
