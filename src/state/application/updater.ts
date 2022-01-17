0c987c375f70f8e042b0ede970112aa837d0d446
specialist0x <specialist0x@protonmail.com>
2022-01-17 14:39:26 -0800

    Cleaned up code

--- src/state/application/updater.ts
+++ src/state/application/updater.ts
@@ -5,10 +5,7 @@ import { updateBlockNumber } from './actions'
 import { useDispatch } from 'react-redux'
 import useTimeout from '../../hooks/useTimeout'
 import useDebounce from '../../hooks/useDebounce'
-import { Web3Provider } from '@ethersproject/providers'
-import { NetworkContextName } from '../../constants'
-import { useWeb3React as useWeb3ReactCore } from '@web3-react/core'
-import { getNetworkLibrary, network } from '../../connectors'
+import { getNetworkLibrary } from '../../connectors'
 
 const MAX_WAIT_BEFORE_MANUAL_DISPATCH = 2000
 
@@ -16,25 +13,20 @@ export default function Updater(): null {
   const { library, chainId } = useActiveWeb3React()
   const networkLibrary = getNetworkLibrary()
 
-  // const test = getNetworkLibrary()
-  // test.on('block', block => {
-  //   console.log('network heard block: ', block)
-  // })
-
   const dispatch = useDispatch()
 
   const windowVisible = useIsWindowVisible()
 
   const [state, setState] = useState<{ chainId: number | undefined; blockNumber: number | null }>({
     chainId,
-    blockNumber: null,
+    blockNumber: null
   })
   const debouncedState = useDebounce(state, 100)
 
   const blockNumberCallback = useCallback(
     (blockNumber: number) => {
-      console.log('Network block update: ', blockNumber)
-      setState((state) => {
+      console.debug('Network block update: ', blockNumber)
+      setState(state => {
         if (chainId === state.chainId) {
           if (typeof state.blockNumber !== 'number') return { chainId, blockNumber }
           return { chainId, blockNumber: Math.max(blockNumber, state.blockNumber) }
@@ -56,28 +48,6 @@ export default function Updater(): null {
   useTimeout(setDelayTimeout, MAX_WAIT_BEFORE_MANUAL_DISPATCH)
 
   // attach/detach listeners
-  // useEffect(() => {
-  //   if (!library || !chainId || !windowVisible) {
-  //     return undefined
-  //   }
-
-  //   setState({ chainId, blockNumber: null })
-
-  //   library
-  //     .getBlockNumber()
-  //     .then(block => {
-  //       console.log('MM block update: ', block)
-  //       blockNumberCallback(block)
-  //     })
-  //     .catch(error => console.error(`Failed to get block number for chainId: ${chainId}`, error))
-
-  //   library.on('block', blockNumberCallback)
-
-  //   return () => {
-  //     library.removeListener('block', blockNumberCallback)
-  //   }
-  // }, [dispatch, chainId, library, blockNumberCallback, windowVisible])
-
   useEffect(() => {
     if (!networkLibrary || !chainId || !windowVisible) {
       return undefined
@@ -88,7 +58,7 @@ export default function Updater(): null {
     networkLibrary
       .getBlockNumber()
       .then(blockNumberCallback)
-      .catch((error) => console.error(`Failed to get block number for chainId: ${chainId}`, error))
+      .catch(error => console.error(`Failed to get block number for chainId: ${chainId}`, error))
 
     networkLibrary.on('block', blockNumberCallback)
 
