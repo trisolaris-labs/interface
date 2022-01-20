9fb40d8d53c156d5261f8a108a992e60bf33fc13
specialist0x <specialist0x@protonmail.com>
2022-01-20 10:35:14 -0800

    Added cdn URL for datav2.json

--- src/fetchers/farms.ts
+++ src/fetchers/farms.ts
@@ -2,9 +2,26 @@ import useSWR from 'swr'
 import { ExternalInfo } from '../state/stake/stake-constants'
 
 async function fetcher() {
-  const response = await fetch('https://raw.githubusercontent.com/trisolaris-labs/apr/master/datav2.json')
+  try {
+    const response = await fetch('https://cdn.trisolaris.io/datav2.json')
 
-  return response.json()
+    return response.json()
+  } catch (e) {
+    console.debug('Error loading datav2.json from cdn.  Falling back to github')
+
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    return await backupFetcher()
+  }
+}
+
+async function backupFetcher() {
+  try {
+    const response = await fetch('https://raw.githubusercontent.com/trisolaris-labs/apr/master/datav2.json')
+
+    return response.json()
+  } catch (e) {
+    console.debug('Error loading datav2.json from github')
+  }
 }
 
 async function getStakingInfoData() {
