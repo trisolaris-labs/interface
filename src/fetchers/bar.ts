9b3f69e237fe59f3e967ee46c3458798f333d9aa
specialist0x <specialist0x@protonmail.com>
2022-02-02 16:08:38 -0800

    Removed github fallback for xtri.json

--- src/fetchers/bar.ts
+++ src/fetchers/bar.ts
@@ -6,20 +6,7 @@ async function fetcher() {
 
     return response.json()
   } catch (e) {
-    console.debug('Error loading datav2.json from cdn.  Falling back to github')
-
-    // eslint-disable-next-line @typescript-eslint/no-use-before-define
-    return await backupFetcher()
-  }
-}
-
-async function backupFetcher() {
-  try {
-    const response = await fetch('https://raw.githubusercontent.com/trisolaris-labs/apr/master/xtri.json')
-
-    return response.json()
-  } catch (e) {
-    console.debug('Error loading datav2.json from github')
+    console.debug('Error loading xtri.json from cdn')
   }
 }
 
