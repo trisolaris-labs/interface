00a18c36d6fb06d4c8e5e9040663eb030b46ac07
specialist0x <specialist0x@protonmail.com>
2022-02-23 14:01:38 -0800

    Refactored pools to remove redundant property definitions

--- src/connectors/__mocks__/NetworkConnector.ts
+++ src/connectors/__mocks__/NetworkConnector.ts
@@ -1,3 +1,5 @@
+class MiniRpcProvider {}
+
 export const NetworkConnector = jest.fn().mockImplementation(
   () =>
     class NetworkConnectorMock {
@@ -5,20 +7,20 @@ export const NetworkConnector = jest.fn().mockImplementation(
         return jest.fn()
       }
 
-      public async activate(): Promise<ConnectorUpdate> {
+      public async activate(): Promise<any> {
         return jest.fn()
       }
 
-      public async getProvider(): Promise<MiniRpcProvider> {
+      public async getProvider(): Promise<any> {
         return jest.fn()
       }
 
-      public async getChainId(): Promise<number> {
+      public async getChainId(): Promise<any> {
         return jest.fn()
       }
 
       public async getAccount(): Promise<null> {
-        return jest.fn()
+        return null
       }
 
       public deactivate() {
