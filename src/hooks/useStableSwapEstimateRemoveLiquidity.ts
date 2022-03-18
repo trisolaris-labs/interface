74eacd945b34602d3c3d5c25e075db292301a81e
specialist0x <specialist0x@protonmail.com>
2022-03-18 13:59:35 -0700

    Cleaned up hook, added error message

--- src/hooks/useStableSwapEstimateRemoveLiquidity.ts
+++ src/hooks/useStableSwapEstimateRemoveLiquidity.ts
@@ -1,4 +1,4 @@
-import { CurrencyAmount, ChainId, BigintIsh, JSBI } from '@trisolaris/sdk'
+import { CurrencyAmount, ChainId, JSBI } from '@trisolaris/sdk'
 import { useState, useCallback } from 'react'
 import { BIG_INT_ZERO } from '../constants'
 import { StableSwapPoolName, STABLESWAP_POOLS } from '../state/stableswap/constants'
@@ -11,41 +11,66 @@ type Props = {
   withdrawTokenIndex: number | null
 }
 
+type TXError = Error & { reason: string }
+
 export default function useStableSwapEstimateRemoveLiquidity({
   amount,
   withdrawTokenIndex,
   stableSwapPoolName
-}: Props): [CurrencyAmount[], () => Promise<void>] {
-  const pool = STABLESWAP_POOLS[ChainId.AURORA][stableSwapPoolName]
-  const poolCurrencies = pool.poolTokens.map(token => unwrappedToken(token))
-  const swapContract = useStableSwapContract(stableSwapPoolName)
-  const initialAmounts = poolCurrencies.map(item => CurrencyAmount.fromRawAmount(item, BIG_INT_ZERO))
+}: Props): [CurrencyAmount[], () => Promise<void>, TXError | null] {
+  const { poolTokens } = STABLESWAP_POOLS[ChainId.AURORA][stableSwapPoolName]
+  const poolCurrencies = poolTokens.map(token => unwrappedToken(token))
+
+  const initialAmounts = poolCurrencies.map(currency => CurrencyAmount.fromRawAmount(currency, BIG_INT_ZERO))
   const [amounts, setAmounts] = useState<CurrencyAmount[]>(initialAmounts)
+  const [error, setError] = useState<TXError | null>(null)
+
+  const swapContract = useStableSwapContract(stableSwapPoolName)
+  const resetState = useCallback(() => {
+    setAmounts(initialAmounts)
+    setError(null)
+  }, [initialAmounts])
+  const lpTokensBeingBurned = amount?.raw.toString() ?? '0'
+
+  const estimateRemovingOneToken = useCallback(
+    (tokenIndex: number) => {
+      return swapContract
+        ?.calculateRemoveLiquidityOneToken(lpTokensBeingBurned, tokenIndex)
+        .then((response: BigInt) => {
+          setAmounts(
+            poolCurrencies.map((currency, i) => {
+              if (i === tokenIndex) {
+                return CurrencyAmount.fromRawAmount(currency, JSBI.BigInt(response))
+              } else {
+                return CurrencyAmount.fromRawAmount(currency, BIG_INT_ZERO)
+              }
+            })
+          )
+        })
+    },
+    [lpTokensBeingBurned, poolCurrencies, swapContract]
+  )
+
+  const estimateRemoveLiquidity = useCallback(() => {
+    return swapContract?.calculateRemoveLiquidity(lpTokensBeingBurned).then((response: BigInt[]) => {
+      setAmounts(
+        poolCurrencies.map((currency, i) => {
+          return CurrencyAmount.fromRawAmount(currency, JSBI.BigInt(response[i]))
+        })
+      )
+    })
+  }, [lpTokensBeingBurned, poolCurrencies, swapContract])
 
   const estimateRemovedLiquidityTokenAmounts = useCallback(async () => {
-    const isRemovingOneToken = withdrawTokenIndex != null
-    let result: BigintIsh | BigintIsh[]
+    resetState()
+    const promise =
+      withdrawTokenIndex != null ? estimateRemovingOneToken(withdrawTokenIndex) : estimateRemoveLiquidity()
 
-    if (isRemovingOneToken) {
-      result = await swapContract?.calculateRemoveLiquidityOneToken(amount?.raw.toString() ?? '0', withdrawTokenIndex)
-    } else {
-      result = await swapContract?.calculateRemoveLiquidity(amount?.raw.toString() ?? '0')
-    }
+    promise.catch((e: TXError) => {
+      console.error('Error estimating removed liquidity: ', e)
+      setError(e)
+    })
+  }, [estimateRemoveLiquidity, estimateRemovingOneToken, resetState, withdrawTokenIndex])
 
-    setAmounts(
-      poolCurrencies.map((currency, i) => {
-        if (isRemovingOneToken) {
-          if (i === withdrawTokenIndex) {
-            return CurrencyAmount.fromRawAmount(currency, JSBI.BigInt(result as BigInt))
-          } else {
-            return CurrencyAmount.fromRawAmount(currency, BIG_INT_ZERO)
-          }
-        } else {
-          return CurrencyAmount.fromRawAmount(currency, JSBI.BigInt((result as BigInt[])[i]))
-        }
-      })
-    )
-  }, [amount?.raw, poolCurrencies, swapContract, withdrawTokenIndex])
-
-  return [amounts, estimateRemovedLiquidityTokenAmounts]
+  return [amounts, estimateRemovedLiquidityTokenAmounts, error]
 }
