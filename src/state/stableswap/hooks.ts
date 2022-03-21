1fe7db5ad78af1705a942c92df69069ce9b6d7dd
Eric Juta <ericjohnjuta@gmail.com>
2022-03-21 16:59:31 +0000

    feat(src/pages/Swap/index.tsx, src/state/stableswap/hooks.ts): add ENABLE_STABLESWAP === 'false' flag conditionals to disable routing via stableswap on swap page especially

--- src/state/stableswap/hooks.ts
+++ src/state/stableswap/hooks.ts
@@ -13,7 +13,7 @@ import { Field, replaceStableSwapState, selectCurrency, setRecipient, switchCurr
 import { StableSwapState } from './reducer'
 import { useUserSlippageTolerance } from '../user/hooks'
 import { useTranslation } from 'react-i18next'
-import { BIG_INT_ZERO } from '../../constants'
+import { BIG_INT_ZERO, ENABLE_STABLESWAP } from '../../constants'
 import { wrappedCurrency } from '../../utils/wrappedCurrency'
 import { StableSwapData, useCalculateStableSwapPairs } from '../../hooks/useCalculateStableSwapPairs'
 import { useStableSwapContract } from '../../hooks/useContract'
@@ -86,6 +86,11 @@ export function calculatePriceImpact(
 }
 
 export function useStableSwapState(): AppState['stableswap'] {
+  if (ENABLE_STABLESWAP === false) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    return useSelector<AppState, AppState['stableswap']>(state => state.stableswap)
+  }
+  // eslint-disable-next-line react-hooks/rules-of-hooks
   return useSelector<AppState, AppState['swap']>(state => state.swap)
 }
 
