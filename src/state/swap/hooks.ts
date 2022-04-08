ccfae9d538ad2dd1b05698fbc01ed1912de90634
Eric Juta <ericjohnjuta@gmail.com>
2022-04-08 17:20:06 +0100

    Remove console.log isStableSwap
    Co-authored-by: specialist0x <94581898+specialist0x@users.noreply.github.com>

--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -186,7 +186,6 @@ export function useDerivedSwapInfo(
     [currencies]
   )
 
-  // console.log({ isStableSwap })
 
   // get link to trade on v1, if a better rate exists
   const v1Trade = undefined
