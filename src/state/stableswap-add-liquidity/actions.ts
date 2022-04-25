3e8ecb91d224ebec35a04384cbfe2d9898818aa5
specialist0x <specialist0x@protonmail.com>
2022-04-25 12:15:51 -0700

    Added support for 5th token

--- src/state/stableswap-add-liquidity/actions.ts
+++ src/state/stableswap-add-liquidity/actions.ts
@@ -4,7 +4,8 @@ export enum Field {
   CURRENCY_0 = 'CURRENCY_0',
   CURRENCY_1 = 'CURRENCY_1',
   CURRENCY_2 = 'CURRENCY_2',
-  CURRENCY_3 = 'CURRENCY_3'
+  CURRENCY_3 = 'CURRENCY_3',
+  CURRENCY_4 = 'CURRENCY_4'
 }
 
 export const typeInput = createAction<{ field: Field; typedValue: string }>('stableswap-add-liquidity/typeInput')
