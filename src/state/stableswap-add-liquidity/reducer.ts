7452b30009cf9351fc699371c94677059e6103fa
specialist0x <specialist0x@protonmail.com>
2022-04-25 12:15:51 -0700

    Added support for 5th token

--- src/state/stableswap-add-liquidity/reducer.ts
+++ src/state/stableswap-add-liquidity/reducer.ts
@@ -6,13 +6,15 @@ export interface StableSwapAddLiquidityState {
   readonly [Field.CURRENCY_1]: string
   readonly [Field.CURRENCY_2]: string
   readonly [Field.CURRENCY_3]: string
+  readonly [Field.CURRENCY_4]: string
 }
 
 const initialState: StableSwapAddLiquidityState = {
   [Field.CURRENCY_0]: '',
   [Field.CURRENCY_1]: '',
   [Field.CURRENCY_2]: '',
-  [Field.CURRENCY_3]: ''
+  [Field.CURRENCY_3]: '',
+  [Field.CURRENCY_4]: ''
 }
 
 export default createReducer<StableSwapAddLiquidityState>(initialState, builder =>
