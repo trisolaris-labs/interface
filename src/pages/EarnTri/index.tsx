2d3fb13c63eda691ed0c30839293b76c74e6bd4a
specialist0x <specialist0x@protonmail.com>
2022-04-26 16:12:30 -0500

    Added env var to gate stable farms

--- src/pages/EarnTri/index.tsx
+++ src/pages/EarnTri/index.tsx
@@ -5,6 +5,7 @@ import FarmBanner from '../../components/earn/FarmBanner'
 import EarnTri from './EarnTri'
 import EarnTriStable from './EarnTriStable'
 import { FarmType } from './FarmType'
+import { ENABLE_STABLE_FARMS } from '../../constants'
 
 const POOLS_ORDER = [5, 11, 8, 30, 7, 0, 1, 2, 3, 4, 15, 17, 18, 19, 20, 21, 22, 23, 24, 26, 27, 28]
 const LEGACY_POOLS = [6, 16, 12, 13, 14, 9, 10]
@@ -20,10 +21,10 @@ export default function Earn({
   return (
     <PageWrapper gap="lg" justify="center">
       <MemoizedFarmBanner />
-      {farmType === FarmType.NORMAL ? (
-        <EarnTri poolsOrder={POOLS_ORDER} legacyPoolsOrder={LEGACY_POOLS} />
-      ) : (
+      {farmType === FarmType.STABLE && ENABLE_STABLE_FARMS ? (
         <EarnTriStable stablePoolsOrder={STABLE_POOLS} />
+      ) : (
+        <EarnTri poolsOrder={POOLS_ORDER} legacyPoolsOrder={LEGACY_POOLS} />
       )}
     </PageWrapper>
   )
