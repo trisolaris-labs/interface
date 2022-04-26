ab2cb1ec9798f8da74dd77162f909f1efb60823a
memacius32 <memacius32@protonmail.com>
2022-04-25 22:29:38 -0300

    Replace token0 and 1 by tokens

--- src/pages/EarnTri/Manage.tsx
+++ src/pages/EarnTri/Manage.tsx
@@ -283,7 +283,6 @@ export default function Manage({
             >
               Withdraw
             </ButtonPrimary>
-
             <ButtonPrimary
               disabled={
                 earnedAmount == null ||
