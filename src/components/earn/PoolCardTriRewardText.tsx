13d8cefe1809f90cd4fd4f76509c619b64a38d4b
specialist0x <specialist0x@protonmail.com>
2022-05-12 12:20:37 -0700

    Fixes control flow for multiple token rewards

--- src/components/earn/PoolCardTriRewardText.tsx
+++ src/components/earn/PoolCardTriRewardText.tsx
@@ -50,45 +50,52 @@ export default function PoolCardTriRewardText({ apr, inStaging, nonTriAPRs }: Pr
     [apr, getTokenByAddress, nonTriAPRs]
   )
 
+  const tooltipContent = useMemo(
+    () => (
+      <ContentWrapper>
+        <AutoColumn style={{ padding: '0.25rem' }}>
+          {tooltipData.map(({ token, apr }) => (
+            <RowBetween key={token.address}>
+              <AutoColumn justify="center" style={{ display: 'inline-flex' }}>
+                <CurrencyLogo currency={token} size={'16px'} />
+                <TYPE.body marginLeft="0.5rem">{token.symbol}</TYPE.body>
+              </AutoColumn>
+              <AutoColumn style={{ marginLeft: '1rem' }}>
+                <TYPE.body>{apr}%</TYPE.body>
+              </AutoColumn>
+            </RowBetween>
+          ))}
+        </AutoColumn>
+      </ContentWrapper>
+    ),
+    [tooltipData]
+  )
+
   if (inStaging) {
     return <TYPE.white textAlign="end">Coming Soon</TYPE.white>
   }
 
-  const nonTriAPRTotal = nonTriAPRs?.reduce((acc, { apr }) => acc + apr, 0) ?? 0
+  const hasTriRewards = apr !== 0
+  const hasNonTriRewards = nonTriAPRs?.some(({ apr }) => apr > 0)
+  const hasOnlyTriRewards = hasTriRewards && !hasNonTriRewards
+  const hasOnlyNonTriRewards = !hasTriRewards && hasNonTriRewards
+  const hasMultipleNonTriRewards = hasNonTriRewards && Number(nonTriAPRs?.length) > 1
 
-  const hasOnlyTriReward = apr > 0 && nonTriAPRTotal === 0
-  if (hasOnlyTriReward || nonTriAPRs == null) {
+  // If only TRI rewards
+  if (hasOnlyTriRewards) {
     return <PoolCardTriSingleCurrencyReward apr={apr} token={TRI[ChainId.AURORA]} />
   }
 
-  const hasOnlyOneNonTriReward = !hasOnlyTriReward && nonTriAPRs?.length === 1
-  if (hasOnlyOneNonTriReward) {
+  // If only 1 non-TRI reward
+  if (hasOnlyNonTriRewards && !hasMultipleNonTriRewards) {
     const [rewardTokenData] = nonTriAPRs
     return (
       <PoolCardTriSingleCurrencyReward apr={rewardTokenData.apr} token={getTokenByAddress(rewardTokenData.address)} />
     )
   }
 
-  const tooltipContent = (
-    <ContentWrapper>
-      <AutoColumn style={{ padding: '0.25rem' }}>
-        {tooltipData.map(({ token, apr }) => (
-          <RowBetween key={token.address}>
-            <AutoColumn justify="center" style={{ display: 'inline-flex' }}>
-              <CurrencyLogo currency={token} size={'16px'} />
-              <TYPE.body marginLeft="0.5rem">{token.symbol}</TYPE.body>
-            </AutoColumn>
-            <AutoColumn style={{ marginLeft: '1rem' }}>
-              <TYPE.body>{Math.round(apr)}%</TYPE.body>
-            </AutoColumn>
-          </RowBetween>
-        ))}
-      </AutoColumn>
-    </ContentWrapper>
-  )
-
-  const totalAPR = nonTriAPRTotal + apr
-
+  // If multiple rewards, render aggregate APR, token logos, and tooltip
+  const totalAPR = (nonTriAPRs ?? []).reduce((acc, { apr: nonTriAPR }) => acc + nonTriAPR, apr)
   return (
     <Popover content={tooltipContent} show={show}>
       <IconWrapper onMouseEnter={open} onMouseLeave={close}>
@@ -96,7 +103,7 @@ export default function PoolCardTriRewardText({ apr, inStaging, nonTriAPRs }: Pr
           <CurrencyLogo currency={token} key={token.address} size={'16px'} style={{ marginRight: '4px' }} />
         ))}
         <TYPE.white marginRight="4px" textAlign="end">
-          {Math.round(totalAPR)}%
+          {totalAPR}%
         </TYPE.white>
         <Info size="16px" />
       </IconWrapper>
