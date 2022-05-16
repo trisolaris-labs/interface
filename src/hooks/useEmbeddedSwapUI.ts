1f42b651b32bc0fe5e2505141ce1308d9f5866b9
specialist0x <specialist0x@protonmail.com>
2022-05-16 13:07:20 -0400

    Added case for handling allowlisted hosts; created banner exclusion const

--- src/hooks/useEmbeddedSwapUI.ts
+++ src/hooks/useEmbeddedSwapUI.ts
@@ -1,3 +1,9 @@
+import { URL_WARNING_DOMAIN_EXCEPTIONS } from '../constants'
+
 export default function useEmbeddedSwapUI() {
-  return window.self !== window.top
+  const isEmbedded = window.self !== window.top
+
+  const isAllowListed = URL_WARNING_DOMAIN_EXCEPTIONS.has(window.top?.location.hostname ?? '')
+
+  return isAllowListed || isEmbedded
 }
