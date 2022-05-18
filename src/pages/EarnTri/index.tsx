db612c83a3235a02bf619e35ca98a6d0a2a551f3
memacius32 <memacius32@protonmail.com>
2022-05-17 22:46:13 -0400

    Remove memoized FarmBanner. Remove FarmTabs

--- src/pages/EarnTri/index.tsx
+++ src/pages/EarnTri/index.tsx
@@ -3,12 +3,11 @@ import { PageWrapper } from '../../components/Page'
 import FarmBanner from '../../components/earn/FarmBanner'
 import EarnTri from './EarnTri'
 
-const MemoizedFarmBanner = React.memo(FarmBanner)
 
 export default function Earn() {
   return (
     <PageWrapper gap="lg" justify="center">
-      <MemoizedFarmBanner />
+      <FarmBanner />
       <EarnTri />
     </PageWrapper>
   )
