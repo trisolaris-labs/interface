0252ae67d70b9e8cb73af8aa6880867131b6c2c9
memacius32 <memacius32@protonmail.com>
2022-05-25 01:38:51 -0400

    Reorganize infobox and Databox. Add toggle for switching between showing data in tokens or rewards

--- src/pages/StakeTri/StakeV2.tsx
+++ src/pages/StakeTri/StakeV2.tsx
@@ -1,18 +1,69 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import { PageWrapper } from '../../components/Page'
 import StakeBox from './StakeBox'
 import MigrateXtri from './MigrateXtri'
 import ClaimPtri from './ClaimPtri'
-import InfoBox from './InfoBox'
+import StatsBox from './StatsBox'
+import { ExternalLink } from '../../theme'
+import { Text } from 'rebass'
+
+import { StyledContainer } from './StatsBox'
+
+const StyledLearnMoreContainer = styled.div`
+  display: flex;
+  align-items: center;
+`
+
+const StyledExternalLink = styled(ExternalLink)`
+  font-weight: 600;
+  color: ${({ theme }) => theme.primaryText1};
+  margin: 2px 0 10px;
+`
+
+const StyledTextinfo = styled.div`
+  margin: 20px 0;
+`
+
+const TopContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+
+const StatsBoxContainer = styled.div`
+  flex: 1;
+  max-width: 55%;
+  width: 100%;
+  height: 100%;
+`
+
+const AboutContainer = styled(StyledContainer)`
+  flex: 1;
+  max-width: 40%;
+  width: 100%;
+`
 
 function StakeV2() {
   return (
-    <PageWrapper gap="lg" justify="center">
-      <InfoBox />
+    <PageWrapper gap="lg">
+      <TopContainer>
+        <StatsBoxContainer>
+          <StatsBox />
+        </StatsBoxContainer>
+        <AboutContainer>
+          <Text fontSize={24} fontWeight={500}>
+            About pTRI
+          </Text>
+          <StyledTextinfo>pTri is the new Trisolaris revenue share Token.</StyledTextinfo>
+          <StyledLearnMoreContainer>
+            <StyledExternalLink href=""> Learn about how it works ↗</StyledExternalLink>
+          </StyledLearnMoreContainer>
+        </AboutContainer>
+      </TopContainer>
       <MigrateXtri />
       <StakeBox />
-      <ClaimPtri /> 
+      <ClaimPtri />
     </PageWrapper>
   )
 }
---
+++ src/pages/StakeTri/StatsBox.tsx
@@ -0,0 +1,115 @@
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { ChainId } from '@trisolaris/sdk'
+
+import { LargeHeaderWhite } from './StakeTriV1'
+import { AutoColumn } from '../../components/Column'
+import { FixedHeightRow } from '../../components/PositionCard/PositionCard.styles'
+import { RowFixed } from '../../components/Row'
+import CurrencyLogo from '../../components/CurrencyLogo'
+import MultipleCurrencyLogo from '../../components/MultipleCurrencyLogo'
+import Toggle from '../../components/Toggle'
+
+import { usePtriStakeInfo } from '../../hooks/usePtri'
+
+import { TRI } from '../../constants/tokens'
+import { STABLESWAP_POOLS } from '../../state/stableswap/constants'
+
+export const StyledContainer = styled(AutoColumn)<{ disabled?: boolean }>`
+background: #0e3f69
+border-radius: 10px;
+width: 100%;
+padding: 2rem 2rem 1rem 2rem;
+height:100%;
+`
+
+const StyledStakedAmount = styled.span`
+  margin-left: 5px;
+`
+
+const PTRI_REWARDS_TOKEN = STABLESWAP_POOLS.USDC_USDT_USN.poolTokens
+
+function StatsBox() {
+  const {
+    totalStakedAmount,
+    totalStakedInUsd,
+    userStaked,
+    userStakedInUsd,
+    userClaimableRewards,
+    userClaimableRewardsInUsd
+  } = usePtriStakeInfo()
+
+  const [rewardsInTokens, setRewardsInTokens] = useState(true)
+
+  return (
+    <StyledContainer>
+      <FixedHeightRow>
+        <RowFixed>
+          <LargeHeaderWhite fontWeight={500} fontSize={32}>
+            APR
+          </LargeHeaderWhite>
+        </RowFixed>
+        <RowFixed>
+          <LargeHeaderWhite fontWeight={500} fontSize={32}>
+            XXX%
+          </LargeHeaderWhite>
+        </RowFixed>
+      </FixedHeightRow>
+
+      <FixedHeightRow marginTop="20px">
+        <RowFixed>Total Staked</RowFixed>
+        <RowFixed>
+          {rewardsInTokens ? (
+            <>
+              <CurrencyLogo currency={TRI[ChainId.AURORA]} size="16px" />
+              <StyledStakedAmount>{totalStakedAmount.toFixed(2)}</StyledStakedAmount>
+            </>
+          ) : (
+            <span>${totalStakedInUsd}</span>
+          )}
+        </RowFixed>
+      </FixedHeightRow>
+      <FixedHeightRow marginTop="6px">
+        <RowFixed>Your deposits</RowFixed>
+        <RowFixed>
+          {rewardsInTokens ? (
+            <>
+              <CurrencyLogo currency={TRI[ChainId.AURORA]} size="16px" />
+              <StyledStakedAmount>{userStaked.toFixed(2)}</StyledStakedAmount>
+            </>
+          ) : (
+            <span>${userStakedInUsd}</span>
+          )}
+        </RowFixed>
+      </FixedHeightRow>
+      <FixedHeightRow marginTop="6px">
+        <RowFixed alignItems="center">Your rewards</RowFixed>
+        {/* <RowFixed>
+          <MultipleCurrencyLogo currencies={PTRI_REWARDS_TOKEN} size={16} />
+          <StyledStakedAmount>{userClaimableRewards.toFixed(2)}</StyledStakedAmount> / $ {userClaimableRewardsInUsd}
+        </RowFixed> */}
+        <RowFixed>
+          {rewardsInTokens ? (
+            <>
+              <MultipleCurrencyLogo currencies={PTRI_REWARDS_TOKEN} size={16} />
+              <StyledStakedAmount>{userClaimableRewards.toFixed(2)}</StyledStakedAmount>
+            </>
+          ) : (
+            <span>${userClaimableRewardsInUsd}</span>
+          )}
+        </RowFixed>
+      </FixedHeightRow>
+      <FixedHeightRow marginTop="20px">
+        <RowFixed>Show data in:</RowFixed>
+        <Toggle
+          isActive={rewardsInTokens}
+          toggle={() => setRewardsInTokens(!rewardsInTokens)}
+          customToggleText={{ off: '$ USD', on: 'Tokens' }}
+          fontSize="12px"
+        />
+      </FixedHeightRow>
+    </StyledContainer>
+  )
+}
+
+export default StatsBox
