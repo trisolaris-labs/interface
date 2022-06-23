527a7a5110945cc01a1216b4cb95e16c5ac5269d
memacius32 <memacius32@protonmail.com>
2022-06-23 12:04:55 -0300

    feat(farms constants): remove nUSD metapool farm from staging. Move aUSDO-USDT to legacy

--- src/state/stake/stake-constants.ts
+++ src/state/stake/stake-constants.ts
@@ -531,7 +531,7 @@ const AURORA_POOLS: StakingTri[] = [
     lpAddress: STABLESWAP_POOLS[StableSwapPoolName.NUSD_USDC_USDT].lpToken.address,
     rewarderAddress: '0xf4ac19e819f5940E92543B544126E7F20b5f6978',
     allocPoint: 1,
-    inStaging: true,
+    inStaging: false,
     stableSwapPoolName: StableSwapPoolName.NUSD_USDC_USDT,
     friendlyFarmName: STABLESWAP_POOLS[StableSwapPoolName.NUSD_USDC_USDT].friendlyName,
     isFeatured: true
