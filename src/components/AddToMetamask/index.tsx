c53937c8e1af1fb4105c4190ce316e4ffa99c02a
memacius32 <memacius32@protonmail.com>
2022-07-25 14:20:07 -0300

    Remove deprecated props

--- src/components/AddToMetamask/index.tsx
+++ src/components/AddToMetamask/index.tsx
@@ -1,26 +1,13 @@
 import React from 'react'
 import { Token } from '@trisolaris/sdk'
-import styled from 'styled-components'
 
-import { StyledMenuButton } from '../StyledMenu'
-import { MouseoverTooltip } from '../Tooltip'
 import { LinkStyledButton } from '../../theme'
 
 import { useActiveWeb3React } from '../../hooks'
 
 import { registerToken } from '../../utils/wallet'
 
-import MetaMaskIcon from '../../assets/images/metamask.png'
-
-const StyledMetaMaskImg = styled.img`
-  display: flex;
-  width: 20px;
-  :hover {
-    cursor: pointer;
-  }
-`
-
-function AddToMetaMaskButton({ token, textOnly, ...otherProps }: { token: Token; textOnly?: boolean }) {
+function AddToMetaMaskButton({ token, ...otherProps }: { token: Token }) {
   const { library } = useActiveWeb3React()
 
   if (!token) {
@@ -34,13 +21,7 @@ function AddToMetaMaskButton({ token, textOnly, ...otherProps }: { token: Token;
   }
 
   return library?.provider?.isMetaMask ? (
-    textOnly ? (
-      <LinkStyledButton onClick={addToken}>Add {symbol} to MetaMask</LinkStyledButton>
-    ) : (
-      <MouseoverTooltip text={`Add ${symbol} to Metamask`}>
-        <StyledMetaMaskImg src={MetaMaskIcon} alt={'MetaMask logo'} onClick={addToken} {...otherProps} />
-      </MouseoverTooltip>
-    )
+    <LinkStyledButton onClick={addToken}>Add {symbol} to MetaMask</LinkStyledButton>
   ) : null
 }
 
