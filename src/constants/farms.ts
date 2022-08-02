62b3286d0b0b72f0d03da7d5ef7633f6b658ed62
specialist0x <94581898+specialist0x@users.noreply.github.com>
2022-08-02 08:25:38 -0700

    Move linear to legacy farms

--- src/constants/farms.ts
+++ src/constants/farms.ts
@@ -1,4 +1,4 @@
-export const DUAL_REWARDS_POOLS = [32, 33, 31, 8, 30, 7, 15, 19, 27, 28, 29, 38, 37]
+export const DUAL_REWARDS_POOLS = [32, 33, 31, 8, 30, 7, 15, 19, 27, 28, 38, 37]
 
 export const TRI_ONLY_REWARDS_POOLS = [5, 11, 0, 3, 4, 26]
 
@@ -6,4 +6,4 @@ export const ECOSYSTEM_POOLS = [20, 24, 34, 36]
 
 export const STABLE_POOLS = [35, 39]
 
-export const LEGACY_POOLS = [1, 2, 6, 16, 12, 13, 14, 9, 10, 21, 22, 17, 18, 23]
+export const LEGACY_POOLS = [29, 1, 2, 6, 16, 12, 13, 14, 9, 10, 21, 22, 17, 18, 23]
