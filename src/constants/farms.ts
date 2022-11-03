bdc8c7c58fd6b2a919969f5aedb94a5be23d1638
memacius32 <memacius32@protonmail.com>
2022-11-03 13:45:45 +0100

    feat(constants/farms): move TRI-STNEAR to legacy

--- src/constants/farms.ts
+++ src/constants/farms.ts
@@ -1,4 +1,4 @@
-export const DUAL_REWARDS_POOLS = [30, 15, 19, 38, 37, 43, 45, 46]
+export const DUAL_REWARDS_POOLS = [30, 15, 19, 38, 43, 45, 46]
 
 export const TRI_ONLY_REWARDS_POOLS = [7, 8, 11, 0, 3, 4, 31, 32, 33]
 
@@ -6,4 +6,4 @@ export const ECOSYSTEM_POOLS = [20, 24, 34, 42, 35]
 
 export const STABLE_POOLS = [47, 39, 44]
 
-export const LEGACY_POOLS = [41, 1, 2, 6, 16, 12, 13, 14, 9, 10, 21, 22, 17, 18, 23, 27, 26, 28, 29, 36, 5, 40]
+export const LEGACY_POOLS = [41, 1, 2, 6, 16, 12, 13, 14, 9, 10, 21, 22, 17, 18, 23, 27, 26, 28, 29, 36, 5, 40, 37]
