505acb64fdee9bb215733603792dce9e480fa0c1
memacius32 <memacius32@protonmail.com>
2022-11-15 13:18:39 +0100

    feat(farms/constants): move meta-stnear to legacy

--- src/constants/farms.ts
+++ src/constants/farms.ts
@@ -2,8 +2,8 @@ export const DUAL_REWARDS_POOLS = [30, 15, 19, 38, 43, 45, 46]
 
 export const TRI_ONLY_REWARDS_POOLS = [7, 8, 11, 0, 3, 4, 31, 32, 33]
 
-export const ECOSYSTEM_POOLS = [20, 24, 34, 42, 35]
+export const ECOSYSTEM_POOLS = [20, 24, 34, 35]
 
 export const STABLE_POOLS = [47, 39, 44]
 
-export const LEGACY_POOLS = [41, 1, 2, 6, 16, 12, 13, 14, 9, 10, 21, 22, 17, 18, 23, 27, 26, 28, 29, 36, 5, 40, 37]
+export const LEGACY_POOLS = [41, 1, 2, 6, 16, 12, 13, 14, 9, 10, 21, 22, 17, 18, 23, 27, 26, 28, 29, 36, 5, 40, 37, 42]
