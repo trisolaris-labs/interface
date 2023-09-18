783e4ab2b913a82b79318f93a95dcb5aa9b314fc
memacius32 <96993065+memacius32@users.noreply.github.com>
2023-09-18 10:44:11 -0300

    Fix connecting eagerly

--- src/components/Web3Provider/index.tsx
+++ src/components/Web3Provider/index.tsx
@@ -1,10 +1,11 @@
 import { Web3ReactProvider } from '@web3-react/core'
 import { Connector } from '@web3-react/types'
-import { network, useConnectors } from '../../connectors'
+import { network, useConnectors, injected, walletConnect } from '../../connectors'
 import { ReactNode, useEffect } from 'react'
 
 const connect = async (connector: Connector) => {
   try {
+    console.log(connector)
     if (connector.connectEagerly) {
       await connector.connectEagerly()
     } else {
@@ -17,7 +18,10 @@ const connect = async (connector: Connector) => {
 
 export default function Web3Provider({ children }: { children: ReactNode }) {
   const connectors = useConnectors(undefined)
+
   useEffect(() => {
+    connect(injected)
+    connect(walletConnect)
     connect(network)
   }, [])
 
