421382a902fc88c709581b34be788b7712f3ce42
Eric Juta <ericjohnjuta@gmail.com>
2023-11-28 12:47:57 +0000

    feat(Tokens): update token symbol matching and logging
    This update modifies the useToken function in src/hooks/Tokens.ts to match tokens based on whether their symbol includes 'USDT' or 'USDC', rather than checking for exact equality. Additionally, console logging has been added for these specific cases.
    
    In src/constants/tokens/index.ts, the names of USDC_E and USDT_E tokens have been updated to include '(Rainbow Bridged)' in their names across all chains.

--- src/hooks/Tokens.ts
+++ src/hooks/Tokens.ts
@@ -140,12 +140,15 @@ export function useToken(tokenAddress?: string): Token | undefined | null {
 
   return useMemo(() => {
     if (token) {
-      if (token.symbol === 'USDC' && token.address === USDC_E[ChainId.AURORA].address) {
-        return USDC_E[ChainId.AURORA]
-      } else if (token.symbol === 'USDT' && token.address === USDT_E[ChainId.AURORA].address) {
+      if (token.symbol?.includes('USDT') && token.address === USDT_E[ChainId.AURORA].address) {
+        console.log('aiaiai')
         return USDT_E[ChainId.AURORA]
+      } else if (token.symbol?.includes('USDC') && token.address === USDC_E[ChainId.AURORA].address) {
+        console.log('dededed')
+        return USDC_E[ChainId.AURORA]
+      } else {
+        return token
       }
-      return token
     }
     if (!chainId || !address) return undefined
     if (decimals.loading || symbol.loading || tokenName.loading) return null
