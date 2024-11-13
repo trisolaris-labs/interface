7e825a5f2a9d4f5fd8ceed702fd38b726049eaa7
Diego Figueroa <diego.figueroa@aurora.dev>
2024-11-13 14:57:08 -0400

    chore: move farms to legacy
    Includes bstn/brrr/ply/nearx-wnear, auUSDT-auUSDC, usdc.e-nstart

--- src/constants/farms.ts
+++ src/constants/farms.ts
@@ -1,12 +1,11 @@
-export const DUAL_REWARDS_POOLS = [30, 19, 38, 43, 46, 51, 52]
+export const DUAL_REWARDS_POOLS = [19, 51, 52]
 
 export const TRI_ONLY_REWARDS_POOLS = [7, 8, 11, 0, 3, 4, 31, 32, 33, 45]
 
-export const ECOSYSTEM_POOLS = [48]
+export const ECOSYSTEM_POOLS = []
 
 export const STABLE_POOLS = [
   47,
-  44,
   53,
   // NOTE - this is actually mcv2 pool id 56 but evie screwed up the lp token when setting up the stableswap pool farms :(
   54
@@ -44,7 +43,13 @@ export const LEGACY_POOLS = [
   35,
   15,
   39,
-  21
+  21,
+  30,
+  38,
+  43,
+  46,
+  44,
+  48
 ]
 
 export const MULTIPLE_REWARD_POOLS = [50]
