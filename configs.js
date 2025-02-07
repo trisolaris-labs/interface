87ef03c22f5e14a6df61ac280c698ff8efa5b4f6
danila <danilabmt@gmail.com>
2025-02-07 16:00:12 +0100

    available_chains_data integration

--- configs.js
+++ configs.js
@@ -1,6 +1,5 @@
 const data_sources = {
     contract_addresses_base_url: 'https://raw.githubusercontent.com/trisolaris-labs/trisolaris_core/refs/heads/main/deployments',
-    // tokens:'https://raw.githubusercontent.com/trisolaris-labs/tokens/turbo/lists/1313161554/list.json',
     tokens:"https://raw.githubusercontent.com/trisolaris-labs/tokens/turbo/lists/1313161554/list.json",
     available_chains_data: 'https://raw.githubusercontent.com/trisolaris-labs/tokens/refs/heads/feature/extandable_chains/lists/chainsData.json',
     
