2612dbc7acdf2048b956856e0831c3b3f36dbf03
danila <danilabmt@gmail.com>
2025-03-04 11:20:14 +0100

    chore:switch to pull token list and available chains from the token repo master branch

--- configs.js
+++ configs.js
@@ -1,11 +1,9 @@
 const data_sources = {
   contract_addresses_base_url:
     'https://raw.githubusercontent.com/trisolaris-labs/trisolaris_core/refs/heads/deployment/tuxa-chain/deployments',
-  tokens:
-    'https://raw.githubusercontent.com/trisolaris-labs/tokens/refs/heads/feature/extandable_chains/lists/tokens.json',
-  // tokens: 'https://raw.githubusercontent.com/trisolaris-labs/tokens/refs/heads/turbo/lists/1313161554/list.json',
+  tokens: 'https://raw.githubusercontent.com/trisolaris-labs/tokens/refs/heads/master/lists/tokens.json',
   available_chains_data:
-    'https://raw.githubusercontent.com/trisolaris-labs/tokens/refs/heads/feature/extandable_chains/lists/chainsData.json'
+    'https://raw.githubusercontent.com/trisolaris-labs/tokens/refs/heads/master/lists/chainsData.json'
 }
 
 module.exports = data_sources
